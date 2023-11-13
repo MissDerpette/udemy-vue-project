fbb85cf42b05e37c888beca389abd98d2c032a24
missderpette <mderpette@gmail.com>
2023-11-13 20:20:22 +0800

    added vue router

---
+++ vue-proj/vue-router/README.md
@@ -0,0 +1,24 @@
+# vue-router
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+### Reference video
+https://www.youtube.com/watch?v=juocv4AtrHo
+
---
+++ vue-proj/vue-router/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import ContactView from '../views/ContactView.vue'
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: ContactView
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
